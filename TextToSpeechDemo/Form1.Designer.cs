fe9566a4321357cd9bba0cea497f9b59e203324b
NasirKhan <nk99technology@.gmail.com>
2021-02-14 15:38:20 +0600

    first commit

---
+++ TextToSpeechDemo/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace TextToSpeechDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SpeechButton = new System.Windows.Forms.Button();
+            this.SoundTrackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SpeedTrackBar = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.VoiceComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SoundTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(483, 241);
+            this.textBox1.TabIndex = 0;
+            // 
+            // SpeechButton
+            // 
+            this.SpeechButton.Location = new System.Drawing.Point(13, 329);
+            this.SpeechButton.Name = "SpeechButton";
+            this.SpeechButton.Size = new System.Drawing.Size(106, 34);
+            this.SpeechButton.TabIndex = 1;
+            this.SpeechButton.Text = "&Speech";
+            this.SpeechButton.UseVisualStyleBackColor = true;
+            this.SpeechButton.Click += new System.EventHandler(this.SpeechButton_Click);
+            // 
+            // SoundTrackBar
+            // 
+            this.SoundTrackBar.Location = new System.Drawing.Point(392, 292);
+            this.SoundTrackBar.Maximum = 100;
+            this.SoundTrackBar.Name = "SoundTrackBar";
+            this.SoundTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.SoundTrackBar.TabIndex = 2;
+            this.SoundTrackBar.Value = 75;
+            this.SoundTrackBar.Scroll += new System.EventHandler(this.SoundTrackBar_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(333, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Volume:";
+            // 
+            // SpeedTrackBar
+            // 
+            this.SpeedTrackBar.Location = new System.Drawing.Point(217, 292);
+            this.SpeedTrackBar.Minimum = -10;
+            this.SpeedTrackBar.Name = "SpeedTrackBar";
+            this.SpeedTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.SpeedTrackBar.TabIndex = 4;
+            this.SpeedTrackBar.Scroll += new System.EventHandler(this.SpeedTrackBar_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(156, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Speed:";
+            // 
+            // VoiceComboBox
+            // 
+            this.VoiceComboBox.FormattingEnabled = true;
+            this.VoiceComboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.VoiceComboBox.Location = new System.Drawing.Point(13, 288);
+            this.VoiceComboBox.Name = "VoiceComboBox";
+            this.VoiceComboBox.Size = new System.Drawing.Size(121, 21);
+            this.VoiceComboBox.TabIndex = 5;
+            this.VoiceComboBox.Text = "Male";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 375);
+            this.Controls.Add(this.VoiceComboBox);
+            this.Controls.Add(this.SpeedTrackBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SoundTrackBar);
+            this.Controls.Add(this.SpeechButton);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Text To Speech Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.SoundTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SpeechButton;
+        private System.Windows.Forms.TrackBar SoundTrackBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar SpeedTrackBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox VoiceComboBox;
+    }
+}
+
---
+++ TextToSpeechDemo/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextToSpeechDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private SpeechSynthesizer synth = new SpeechSynthesizer();
+        private void SpeechButton_Click(object sender, EventArgs e)
+        {
+            if (VoiceComboBox.Text == "Male")
+            {
+                synth.SelectVoiceByHints(VoiceGender.Male);
+            }
+
+            if (VoiceComboBox.Text == "Female")
+            {
+                synth.SelectVoiceByHints(VoiceGender.Female);
+            }
+            
+            synth.Speak(textBox1.Text);
+        }
+
+        private void SoundTrackBar_Scroll(object sender, EventArgs e)
+        {
+            synth.Volume = SoundTrackBar.Value;
+        }
+
+        private void SpeedTrackBar_Scroll(object sender, EventArgs e)
+        {
+            synth.Rate = SpeedTrackBar.Value;
+        }
+
+        
+    }
+}
